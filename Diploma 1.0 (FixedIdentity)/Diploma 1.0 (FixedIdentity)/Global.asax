92a6a7f8f3126f1dd09ceb113603d06f21c62354
Fuhrick <danchenko15@gmail.com>
2016-10-18 23:48:52 +0300

    Project
    Fixed Asp.net Identity for my diploma

---
+++ Diploma 1.0 (FixedIdentity)/Diploma 1.0 (FixedIdentity)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Diploma_1._0__FixedIdentity_.MvcApplication" Language="C#" %>
---
+++ Diploma 1.0 (FixedIdentity)/Diploma 1.0 (FixedIdentity)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Diploma_1._0__FixedIdentity_.Startup))]
+namespace Diploma_1._0__FixedIdentity_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
